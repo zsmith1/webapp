30ab300b7cac1d0be09002aee9789de7a48d4240
Zach Smith <zach.smith2171@gmail.com>
2023-12-22 12:15:23 -0400

    minor changes

--- routes/index.js
+++ routes/index.js
@@ -18,19 +18,9 @@ async function fetchAllRepos(user) {
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  var rows = []
-  var repoData = []
   fetchAllRepos("zsmith1").then(repos => {
-    for (var i=0; i < repos.data.length; i++){
-      repoData.push(repos.data[i])
-      if (repoData.length == 3){
-        rows.push(repoData)
-        repoData = []
-      }
-    }
-    rows.push(repoData)
     res.render('pages/index', {
-      rows: rows,
+      repos: repos.data,
       languages: languages,
       iac: iac,
       cicd: cicd,
