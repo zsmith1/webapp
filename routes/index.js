58bc5c8868df2855efdabec9bb0e9b08e8e4b1d1
Zach Smith <zach.smith2171@gmail.com>
2024-01-23 16:33:00 -0400

    Switch to AWS lightsail, remove node_modules from git

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,11 @@ var router = express.Router();
 const languages = ["Python", "SQL", "Java", "Javascript", "Go"];
 const iac = ["Ansible", "Terraform", "AWS/Azure"];
 const cicd = ["Gitlab CI", "CircleCI", "Jenkins", "AgroCD"]
+const testing = ["Spock", "Pytest", "Selenium/Geb", "Behave"]
 const other =["Kubernetes", "Docker", "Git", "Agile Development", "Trunk-based Development", "Jira/Confluence", "Grafana", "ELK Stack"];
 
 const { Octokit } = require("@octokit/core");
-const octokit = new Octokit();
+const octokit = new Octokit()
 
 async function fetchAllRepos(user) {
     const result = await octokit.request('GET /users/{user}/repos', {
@@ -16,16 +17,28 @@ async function fetchAllRepos(user) {
     return result;
 }
 
+const schedule = require('node-schedule');
+
+let repos
+fetchAllRepos('zsmith1').then(it => {
+  repos = it
+});
+
+schedule.scheduleJob('*/5 * * * *', function(){
+  fetchAllRepos('zsmith1').then(it => {
+    repos = it
+  });
+});
+
 /* GET home page. */
-router.get('/', function(req, res) {
-  fetchAllRepos('zsmith1').then(repos => {
-    res.render('pages/index', {
-      repos: repos.data,
-      languages: languages,
-      iac: iac,
-      cicd: cicd,
-      other: other
-    });
+router.get('/', async function(req, res) {
+  res.render('pages/index', {
+    repos: repos.data,
+    languages: languages,
+    iac: iac,
+    cicd: cicd,
+    testing: testing,
+    other: other
   });
 });
 
